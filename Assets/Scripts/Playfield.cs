fa889c2d28a34d5c643ae64b6de22ceecf1597a9
Andres Tovar <andres.tovar@king.com>
2020-06-05 12:35:09 +0200

    First commit

---
+++ Assets/Scripts/Playfield.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playfield : MonoBehaviour
+{
+	[SerializeField]
+	private int columns;
+
+	[SerializeField]
+	private int rows;
+	
+	private List<List<int>> grid = new List<List<int>>();
+
+	private void Awake()
+	{
+
+	}
+}
